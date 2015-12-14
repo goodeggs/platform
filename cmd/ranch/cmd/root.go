ddc33c90677ce85eb5548606ef94d1288dc0db4d
Bob Zoller <bob@zoller.us>
2015-12-14 13:17:26 -0800

    remove crufty option

--- cmd/ranch/cmd/root.go
+++ cmd/ranch/cmd/root.go
@@ -11,20 +11,14 @@ import (
 var cfgFile string
 var App string
 
-// This represents the base command when called without any subcommands
 var RootCmd = &cobra.Command{
 	Use:   "ranch",
 	Short: "Ranch CLI",
 	Long: `A CLI interface to Ranch aka the Good Eggs platform,
   maintained with love by the Delivery Engineering team in Go.
   More information is available at https://github.com/goodeggs/platform`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	//	Run: func(cmd *cobra.Command, args []string) { },
 }
 
-// Execute adds all child commands to the root command sets flags appropriately.
-// This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
 	if err := RootCmd.Execute(); err != nil {
 		fmt.Println(err)
@@ -34,19 +28,10 @@ func Execute() {
 
 func init() {
 	cobra.OnInitialize(initConfig)
-
-	// Here you will define your flags and configuration settings.
-	// Cobra supports Persistent Flags, which, if defined here,
-	// will be global for your application.
-
 	RootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.ranch.yaml)")
 	RootCmd.PersistentFlags().StringVarP(&App, "app", "a", "", "application name (defaults to CWD)")
-	// Cobra also supports local flags, which will only run
-	// when this action is called directly.
-	RootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
 
-// initConfig reads in config file and ENV variables if set.
 func initConfig() {
 	if cfgFile != "" { // enable ability to specify config file via flag
 		viper.SetConfigFile(cfgFile)
