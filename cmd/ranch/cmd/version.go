4a707eac8bddbf7bfc8bd00a91c689995fa263f7
Bob Zoller <bob@zoller.us>
2015-12-14 15:43:24 -0800

    adds ranch version command

---
+++ cmd/ranch/cmd/version.go
@@ -0,0 +1,69 @@
+package cmd
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"regexp"
+	"strings"
+
+	"github.com/goodeggs/platform/cmd/ranch/util"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Prints the application version",
+	Run: func(cmd *cobra.Command, args []string) {
+		version, err := util.AppVersion(cmd)
+		util.Check(err)
+
+		fmt.Printf("v%d\n", version)
+	},
+}
+
+var versionBumpCmd = &cobra.Command{
+	Use:   "bump",
+	Short: "Increments the application version",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		oldVersion, err := util.AppVersion(cmd)
+		util.Check(err)
+
+		newVersion := oldVersion + 1
+
+		appConfigPath, err := util.AppConfigPath(cmd)
+		util.Check(err)
+
+		input, err := ioutil.ReadFile(appConfigPath)
+		util.Check(err)
+
+		// fuzzy match ^version: 1$
+		re, err := regexp.Compile(fmt.Sprintf("^\\s*version\\s*:\\s*%d\\b", oldVersion))
+		util.Check(err)
+
+		scanner := bufio.NewScanner(bytes.NewReader(input))
+		var buf bytes.Buffer
+		var line string
+		for scanner.Scan() {
+			line = scanner.Text()
+			if re.Match([]byte(line)) {
+				line = strings.Replace(line, fmt.Sprintf("%d", oldVersion), fmt.Sprintf("%d", newVersion), 1)
+			}
+			buf.WriteString(line)
+			buf.WriteString("\n")
+		}
+		util.Check(scanner.Err())
+
+		err = ioutil.WriteFile(appConfigPath, buf.Bytes(), 0644)
+		util.Check(err)
+
+		fmt.Printf("v%d\n", newVersion)
+	},
+}
+
+func init() {
+	versionCmd.AddCommand(versionBumpCmd)
+	RootCmd.AddCommand(versionCmd)
+}
