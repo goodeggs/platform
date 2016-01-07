6b8a2d2abcfbaac50fc25b9e4928d543a347908c
Bob Zoller <bob@zoller.us>
2016-01-07 14:13:40 -0800

    'ranch deploy' works

--- cmd/ranch/util/convox.go
+++ cmd/ranch/util/convox.go
@@ -2,8 +2,14 @@ package util
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
+	"path"
+	"time"
 
 	"github.com/convox/rack/client"
+	"github.com/jhoonb/archivex"
 	"github.com/spf13/viper"
 )
 
@@ -16,6 +22,156 @@ func convoxClient() (*client.Client, error) {
 	password := viper.GetString("convox.password")
 	version := "20151211151200"
 	return client.New(host, password, version), nil
+}
+
+func ConvoxPromote(appName string, releaseId string) error {
+	client, err := convoxClient()
+
+	if err != nil {
+		return err
+	}
+
+	_, err = client.PromoteRelease(appName, releaseId)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func ConvoxDeploy(appName string, buildDir string) (string, error) {
+	client, err := convoxClient()
+
+	if err != nil {
+		return "", err
+	}
+
+	app, err := client.GetApp(appName)
+
+	if err != nil {
+		return "", err
+	}
+
+	switch app.Status {
+	case "creating":
+		return "", fmt.Errorf("app is still creating: %s", appName)
+	case "running", "updating":
+	default:
+		return "", fmt.Errorf("unable to build app: %s", appName)
+	}
+
+	tar, err := createTarball(buildDir)
+
+	if err != nil {
+		return "", err
+	}
+
+	cache := true
+	config := "docker-compose.yml"
+
+	build, err := client.CreateBuildSource(appName, tar, cache, config)
+
+	if err != nil {
+		return "", err
+	}
+
+	return finishBuild(client, appName, build)
+}
+
+func createTarball(buildDir string) ([]byte, error) {
+	tmpDir, err := ioutil.TempDir("", "ranch")
+	defer os.RemoveAll(tmpDir)
+	fmt.Println(tmpDir)
+
+	if err != nil {
+		return nil, err
+	}
+
+	tgzfile := path.Join(tmpDir, "build.tar.gz")
+
+	tar := new(archivex.TarFile)
+
+	err = tar.Create(tgzfile)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tar.AddAll(buildDir, false)
+	if err != nil {
+		return nil, err
+	}
+
+	err = tar.Close()
+	if err != nil {
+		return nil, err
+	}
+
+	return ioutil.ReadFile(tgzfile)
+}
+
+func finishBuild(client *client.Client, appName string, build *client.Build) (string, error) {
+
+	if build.Id == "" {
+		return "", fmt.Errorf("unable to fetch build id")
+	}
+
+	reader, writer := io.Pipe()
+	go io.Copy(os.Stdout, reader)
+	err := client.StreamBuildLogs(appName, build.Id, writer)
+
+	if err != nil {
+		return "", err
+	}
+
+	return waitForBuild(client, appName, build.Id)
+}
+
+func waitForBuild(client *client.Client, appName, buildId string) (string, error) {
+	for {
+		build, err := client.GetBuild(appName, buildId)
+
+		if err != nil {
+			return "", err
+		}
+
+		switch build.Status {
+		case "complete":
+			return build.Release, nil
+		case "error":
+			return "", fmt.Errorf("%s build failed", appName)
+		case "failed":
+			return "", fmt.Errorf("%s build failed", appName)
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return "", fmt.Errorf("can't get here")
+}
+
+func WaitForStatus(appName, status string) error {
+	client, err := convoxClient()
+
+	if err != nil {
+		return err
+	}
+
+	for {
+		app, err := client.GetApp(appName)
+
+		if err != nil {
+			return err
+		}
+
+		if app.Status == status {
+			return nil
+		}
+
+		time.Sleep(1 * time.Second)
+	}
+
+	return fmt.Errorf("can't get here")
 }
 
 func ConvoxPs(appName string) (Processes, error) {
--- cmd/ranch/util/docker.go
+++ cmd/ranch/util/docker.go
@@ -3,6 +3,7 @@ package util
 import (
 	"fmt"
 	"os"
+	"strings"
 
 	"github.com/fsouza/go-dockerclient"
 )
@@ -16,7 +17,34 @@ func dockerClient() (*docker.Client, error) {
 }
 
 func DockerPush(imageName string) error {
-	fmt.Println("TODO: docker push $imageName")
+	parts := strings.Split(imageName, ":")
+	name, tag := parts[0], parts[1]
+
+	client, err := dockerClient()
+
+	if err != nil {
+		return err
+	}
+
+	opts := docker.PushImageOptions{
+		Name:         name,
+		Tag:          tag,
+		Registry:     "https://index.docker.io/v1/",
+		OutputStream: os.Stdout,
+	}
+
+	auths, err := docker.NewAuthConfigurationsFromDockerCfg()
+
+	if err != nil {
+		return err
+	}
+
+	err = client.PushImage(opts, auths.Configs["https://index.docker.io/v1/"])
+
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
 
@@ -31,11 +59,18 @@ func DockerBuild(appDir string, imageName string) error {
 		return err
 	}
 
+	auth, err := docker.NewAuthConfigurationsFromDockerCfg()
+
+	if err != nil {
+		return err
+	}
+
 	opts := docker.BuildImageOptions{
 		Name:         imageName,
 		OutputStream: os.Stdout,
 		ContextDir:   appDir,
 		Pull:         true,
+		AuthConfigs:  *auth,
 	}
 
 	return client.BuildImage(opts)
