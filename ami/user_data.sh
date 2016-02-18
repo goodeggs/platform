8613f596ffad682a457cb67093f33e2d3a0bf4a0
Bob Zoller <bob@zoller.us>
2016-02-18 09:46:46 -0800

    scp works fine on source_ami

--- ami/user_data.sh
+++ ami/user_data.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-# NOTE: this exists because the ECS AMI does not include scp.
-# we can kill this and the hacky sleep 30 off once https://github.com/mitchellh/packer/pull/2504 is merged.
 
-yum install -y openssh-clients
