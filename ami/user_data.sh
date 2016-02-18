c80f9073e7401eec1663e17ae262516ce27f5b3e
Bob Zoller <bob@zoller.us>
2016-02-18 09:47:45 -0800

    disable cloud-init during packer builds

--- ami/user_data.sh
+++ ami/user_data.sh
@@ -1,3 +1,21 @@
-#!/bin/sh
+#cloud-config
 
+# attempt to disable everything except ssh and the initial yum update
+
+cloud_init_modules:
+  - users-groups
+  - ssh
+
+cloud_config_modules:
+  - yum-configure
+  - yum-add-repo
+  - package-update-upgrade-install
+
+packages: []
+
+cloud_final_modules: []
+
+bootcmd: []
+
+runcmd: []
 
