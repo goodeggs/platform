a041f9241582ae06eef82d073d1c1fef2665a46c
Bob Zoller <bob@zoller.us>
2016-06-01 23:43:43 -0700

    move version into its own file

--- cmd/ranch/main.go
+++ cmd/ranch/main.go
@@ -4,24 +4,16 @@ import (
 	"fmt"
 	"os"
 
-	"github.com/goodeggs/platform/cmd/ranch/Godeps/_workspace/src/github.com/spf13/cobra"
 	"github.com/goodeggs/platform/cmd/ranch/cmd"
 )
 
 var VERSION = "dev"
 
-var versionCmd = &cobra.Command{
-	Use:   "version",
-	Short: "Prints the ranch CLI version",
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println(VERSION)
-	},
+func init() {
+	cmd.Version = VERSION
 }
 
 func main() {
-	cmd.Version = VERSION
-	cmd.RootCmd.AddCommand(versionCmd)
-
 	if err := cmd.RootCmd.Execute(); err != nil {
 		if err.Error() == "pflag: help requested" {
 			cmd.RootCmd.Usage()
