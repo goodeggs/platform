goodeggs/platform

commit 1df12b1a1c17c40c7c8c89fea0b29c8d25be1351

deploy handles re-deploying existing versions

cmd/ranch/cmd/deploy.go
@@ -30,6 +30,7 @@ USER app
 ENV HOME /app
 WORKDIR /app
 
+COPY node_modules /build/node_modules
 COPY . /build
 
 RUN sudo mkdir -p /cache && \
@@ -104,14 +105,7 @@ var deployCmd = &cobra.Command{
 
 		imageNameWithTag := strings.Join([]string{config.ImageName, appVersion}, ":")
 
-		exists, err := util.RanchReleaseExists(config.AppName, appVersion)
-		if err != nil {
-			return err
-		} else if exists {
-			return fmt.Errorf("release %s already exists. use `ranch panic:rollback %s -a %s` instead", appVersion, appVersion, appName)
-		}
-
-		exists, err = util.DockerImageExists(imageNameWithTag)
+		exists, err := util.DockerImageExists(imageNameWithTag)
 		if err != nil {
 			return err
 		} else if exists {
@@ -122,32 +116,54 @@ var deployCmd = &cobra.Command{
 			}
 		}
 
-		buildDir, err := ioutil.TempDir("", "ranch")
+		exists, err = util.RanchReleaseExists(config.AppName, appVersion)
 		if err != nil {
 			return err
-		}
-
-		fmt.Println("using build directory", buildDir)
-
-		var env map[string]string
-		if config.EnvId != "" {
-			plaintext, err := util.RanchGetSecret(appName, config.EnvId)
+		} else if exists {
+			currentVersion, err := util.ConvoxCurrentVersion(config.AppName)
 			if err != nil {
 				return err
 			}
 
-			env, err = util.ParseEnv(plaintext)
+			if currentVersion != appVersion {
+				fmt.Printf("promoting existing release %s\n", appVersion)
+				if err = util.ConvoxPromote(config.AppName, appVersion); err != nil {
+					return err
+				}
+				if err = util.ConvoxWaitForStatus(config.AppName, "running"); err != nil {
+					return err
+				}
+			} else {
+				fmt.Printf("existing release %s is currently live, skipping promote.\n", appVersion)
+			}
+		} else {
+			buildDir, err := ioutil.TempDir("", "ranch")
 			if err != nil {
 				return err
 			}
-		}
 
-		if err = generateDockerCompose(imageNameWithTag, config, env, buildDir); err != nil {
-			return err
-		}
+			fmt.Println("using build directory", buildDir)
 
-		if err = convoxDeploy(appName, appVersion, buildDir); err != nil {
-			return err
+			var env map[string]string
+			if config.EnvId != "" {
+				plaintext, err := util.RanchGetSecret(appName, config.EnvId)
+				if err != nil {
+					return err
+				}
+
+				env, err = util.ParseEnv(plaintext)
+				if err != nil {
+					return err
+				}
+			}
+
+			if err = generateDockerCompose(imageNameWithTag, config, env, buildDir); err != nil {
+				return err
+			}
+
+			if err = convoxDeploy(appName, appVersion, buildDir); err != nil {
+				return err
+			}
 		}
 
 		if err = util.ConvoxScale(appName, config); err != nil {

cmd/ranch/cmd/panic_rollback.go
@@ -23,6 +23,8 @@ var panicRollbackCmd = &cobra.Command{
 
 		appVersion := args[0]
 
+		fmt.Printf("promoting release %s", appVersion)
+
 		if err = util.ConvoxPromote(appName, appVersion); err != nil {
 			return err
 		}

cmd/ranch/util/convox.go
@@ -206,6 +206,30 @@ func ConvoxScale(appName string, config *RanchConfig) (err error) {
 	return nil
 }
 
+func ConvoxCurrentVersion(appName string) (string, error) {
+	client, err := convoxClient()
+	if err != nil {
+		return "", err
+	}
+
+	app, err := client.GetApp(appName)
+	if err != nil {
+		return "", err
+	}
+
+	shaMap, err := buildShaMap(appName)
+	if err != nil {
+		return "", err
+	}
+
+	sha, exists := shaMap[app.Release]
+	if !exists {
+		return "", fmt.Errorf("current running an unknown convox release %s", app.Release)
+	}
+
+	return sha, nil
+}
+
 func ConvoxPromote(appName string, appVersion string) error {
 	releaseId, err := getConvoxRelease(appName, appVersion)
 

commit 49e250fa65234f7c9658a00fd61666f7c24470e2

fix deploy bug introduced in 1df12b1a

cmd/ranch/cmd/deploy.go
@@ -30,7 +30,6 @@ USER app
 ENV HOME /app
 WORKDIR /app
 
-COPY node_modules /build/node_modules
 COPY . /build
 
 RUN sudo mkdir -p /cache && \