799879a3e721e3c2755a4d02fed5315525afd8bc
Keith Swett <swettk@gmail.com>
2017-05-18 15:41:04 +0000

    Implementing report and reaper for misbehaving containers
    We now report via syslog the number of major page faults per
    container.  Additionally, if a container goes over 10000 major
    page faults we docker kill it.

--- ami/adjust-cgroup-settings.sh
+++ ami/adjust-cgroup-settings.sh
@@ -31,24 +31,31 @@ for a in $TARGETS; do
   cat "./memory.limit_in_bytes" > "./memory.memsw.limit_in_bytes"
   popd
 
-  # Keeping here for now incase we leave it later
-  # Limit IOPS for all devices (5 IOPS/Sec to prevent thrashing, reads and writes)
-  #pushd /cgroup/blkio/docker/$a
-  #DEVICES="$(cat ./blkio.throttle.io_service_bytes  | awk '{print $1;}' | uniq | grep -v Total)"
-  #for b in $DEVICES; do
-  #  echo "${b} 5" > ./blkio.throttle.write_iops_device
-  #  echo "${b} 5" > ./blkio.throttle.read_iops_device
-  #done
-  #popd
+  # Limit IOPS for all devices (150 IOPS/Sec to prevent thrashing, reads and writes)
+  pushd /cgroup/blkio/docker/$a
+  DEVICES="$(cat ./blkio.throttle.io_service_bytes  | awk '{print $1;}' | uniq | grep -v Total)"
+  for b in $DEVICES; do
+    echo "${b} 150" > ./blkio.throttle.write_iops_device
+    echo "${b} 150" > ./blkio.throttle.read_iops_device
+  done
+  popd
 done
 
 # No more swap
-# /sbin/swapoff -a
+/sbin/swapoff -a
 
 # Log major pagefaults
 for a in $TARGETS; do
   pushd /cgroup/memory/docker/$a
   RESULT=$(echo "id=${a} $(docker inspect --format 'image={{.Config.Image}} StartedAt="{{.State.StartedAt}}"' $a) pgmajfault=$(cat memory.stat | grep total_pgmajfault | awk '{print $2;}')")
+  MEASURE=$(echo ${RESULT} | awk '{ print $4;}' | sed 's/pgmajfault=//g')
+
+  if [[ ${MEASURE} -gt 10000 ]]; then
+    echo "Executing docker kill on misbehaving container $a due to ${MEASURE} major page faults over 10000"
+    logger "Executing docker kill on misbehaving container $a due to ${MEASURE} major page faults over 10000"
+    docker kill $a
+  fi
+
   logger $RESULT
   echo $RESULT
   popd
